83cff0666201cdf68d4a1665ffb5dc9b1159ea40
rarlala <wtw9611@gmail.com>
2024-04-24 16:30:56 +0900

    feat: Video 추가

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,12 +1,24 @@
-// import style from './Video.module.scss'
+import style from './Video.module.scss'
 import classNames from 'classnames/bind'
 
 import Section from '@shared/Section'
 
-// const cx = classNames.bind(style)
+const cx = classNames.bind(style)
 
 function Video() {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video
+        className={cx('video')}
+        poster="/assets/poster.jpg"
+        muted
+        autoPlay
+        loop
+      >
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  )
 }
 
 export default Video
